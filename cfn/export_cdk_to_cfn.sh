45718f68a3df0eeac0346cd6f0357d4d0de41420
Dave Thibault <davetbo@amazon.com>
2024-10-31 20:45:50 +0000

    cloudformation export

--- cfn/export_cdk_to_cfn.sh
+++ cfn/export_cdk_to_cfn.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
-rm -Rf ../cfn/backend/* && \
-rm -Rf ../cfn/frontend/* && \
+if [ ! -d ../cfn/files ]
+then
+  mkdir -p ../cfn/files
+fi
+
+rm -Rf ../cfn/files/* && \
+cp ../cfn/codebuild-stack-template.yaml ../cfn/files
+
 rm -Rf ../backend/cdk.out && \
 sudo rm -Rf ../frontend/cdk.out && \
 cd ../backend && \
 
 # previously had cdk synth -e --ci --no-staging but removing for troubleshooting.
-cdk synth mtfsrad-b-dev | sed '/CDKMetadata/,$d' > ../cfn/backend/mtfsrad-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/AuthProviderStack | sed '/CDKMetadata/,$d' > ../cfn/backend/auth-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/Vpc | sed '/CDKMetadata/,$d' > ../cfn/backend/vpc-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/BedrockProviderStack | sed '/CDKMetadata/,$d' > ../cfn/backend/bedrock-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/EmbeddingsProviderStack | sed '/CDKMetadata/,$d' > ../cfn/backend/embeddings-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/IngestionProviderStack | sed '/CDKMetadata/,$d' > ../cfn/backend/ingestion-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/VectorStoreProviderStack | sed '/CDKMetadata/,$d' > ../cfn/backend/vector-store-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/DocumentCollectionsHandlerStack | sed '/CDKMetadata/,$d' > ../cfn/backend/doc-collections-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/PromptTemplateHandlerStack | sed '/CDKMetadata/,$d' > ../cfn/backend/prompt-templates-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/GraphStoreProviderStack | sed '/CDKMetadata/,$d' > ../cfn/backend/graph-store-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/EnrichmentPipelinesHandlerStack | sed '/CDKMetadata/,$d' > ../cfn/backend/enrichment-pipelines-stack-template.yaml && \
-cdk synth mtfsrad-b-dev/GenerationHandlerApiStack | sed '/CDKMetadata/,$d' > ../cfn/backend/generation-handler-stack-template.yaml && \
+cdk synth mtfsrad-b-dev | sed '/CDKMetadata/,$d' > ../cfn/files/mtfsrad-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/AuthProviderStack | sed '/CDKMetadata/,$d' > ../cfn/files/auth-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/Vpc | sed '/CDKMetadata/,$d' > ../cfn/files/vpc-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/BedrockProviderStack | sed '/CDKMetadata/,$d' > ../cfn/files/bedrock-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/EmbeddingsProviderStack | sed '/CDKMetadata/,$d' > ../cfn/files/embeddings-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/IngestionProviderStack | sed '/CDKMetadata/,$d' > ../cfn/files/ingestion-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/VectorStoreProviderStack | sed '/CDKMetadata/,$d' > ../cfn/files/vector-store-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/DocumentCollectionsHandlerStack | sed '/CDKMetadata/,$d' > ../cfn/files/doc-collections-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/PromptTemplateHandlerStack | sed '/CDKMetadata/,$d' > ../cfn/files/prompt-templates-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/GraphStoreProviderStack | sed '/CDKMetadata/,$d' > ../cfn/files/graph-store-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/EnrichmentPipelinesHandlerStack | sed '/CDKMetadata/,$d' > ../cfn/files/enrichment-pipelines-stack-template.yaml && \
+cdk synth mtfsrad-b-dev/GenerationHandlerApiStack | sed '/CDKMetadata/,$d' > ../cfn/files/generation-handler-stack-template.yaml && \
 
 cd ../frontend && \
 export BUILD_UID=$UID && \
-cdk synth mtfsrad-f-dev | sed '/CDKMetadata/,$d' > ../cfn/frontend/ui-stack-template.yaml && \
+cdk synth mtfsrad-f-dev | sed '/CDKMetadata/,$d' > ../cfn/files/ui-stack-template.yaml && \
 export OUTPUT_BUCKET=ws-assets-us-east-1 && \
 export S3_PREFIX=e03d64e5-c828-4f3b-8143-98a7e3dd7978 && \
 cd ../cfn && \
-find ./ -name '*.yaml' | xargs grep cdk-hnb659fds-assets | cut -d ' ' -f 1 | uniq | python upload_stack_files_on_export.py && \
+find ./files -name '*.yaml' | xargs grep cdk-hnb659fds-assets | cut -d ' ' -f 1 | uniq | python upload_stack_files_on_export.py && \
 git add ../cfn && \
 git commit -m 'cloudformation export' && \
 git push origin main && \
--- cfn/upload_stack_files_on_export.py
+++ cfn/upload_stack_files_on_export.py
@@ -18,9 +18,15 @@ SOURCE_BUCKET = BUCKET_TO_FIND.replace('${AWS::AccountId}', ACCT).replace('${AWS
 print(f"SOURCE_BUCKET: {SOURCE_BUCKET}")
 print("Updating files...")
 
+
+file_manifest = []
+
+
 def download_file(old_s3_key):
-    local_file = f"./backend/{old_s3_key}"
+    global file_manifest
+    local_file = f"./files/{old_s3_key}"
     print(f"downloading s3://{SOURCE_BUCKET}/{old_s3_key} to {local_file}")
+    file_manifest.append(local_file)
     s3.download_file(
         SOURCE_BUCKET,
         old_s3_key,
@@ -33,50 +39,37 @@ for filename in sys.stdin:
     with open(filename, 'r') as f:
         lines = f.readlines()
     
-    output_content = ''
     found_bucket = False
-
-    if filename in [
-        "./backend/bedrock-stack-template.yaml",
-        "./backend/doc-collections-stack-template.yaml",
-    ]:
-        # nothing to change in these
-        pass
-
-    else:
-        # these need to have assets from sub-stacks collected
-        # for upload to github.
-        i = 0
-        while i < len(lines):
-            line = lines[i]
-            if f"Fn::Sub: {BUCKET_TO_FIND}" in line:
-                found_bucket = True
-                # line = line.replace(f"Fn::Sub: {BUCKET_TO_REPLACE}", input_values['output_bucket'])
-                # output_content += line + "\n"
-            elif found_bucket ==True:
-                # the next line after finding the bucket line should land here.
-                old_s3_keys = []
-                old_s3_key = ''
-                if 'S3Key: ' in line:
-                    old_s3_keys.append(line.split('S3Key: ')[1].strip())
-                elif '- /' in line:
-                    new_key = line.replace('- /', '').strip()
-                    if new_key != '*':
-                        old_s3_keys.append(new_key)
-                elif 'SourceObjectKeys:' in line:
+    i = 0
+    
+    while i < len(lines):
+        line = lines[i]
+        if f"Fn::Sub: {BUCKET_TO_FIND}" in line:
+            found_bucket = True
+        elif found_bucket ==True:
+            # the next line after finding the bucket line should land here.
+            old_s3_keys = []
+            old_s3_key = ''
+            if 'S3Key: ' in line:
+                old_s3_keys.append(line.split('S3Key: ')[1].strip())
+            elif '- /' in line:
+                new_key = line.replace('- /', '').strip()
+                if new_key != '*':
+                    old_s3_keys.append(new_key)
+            elif 'SourceObjectKeys:' in line:
+                i += 1
+                while lines[i].startswith('- '):
+                    old_s3_keys.append(lines[i].replace('- ', '').strip())
                     i += 1
-                    while lines[i].startswith('- '):
-                        old_s3_keys.append(lines[i].replace('- ', '').strip())
-                        i += 1
-                    # i -= 1
-                    
-                # now copy the file to the output bucket and target s3 key
-                for key in old_s3_keys:
-                    print(f"found resource to download: {key} in file {filename}. Downloading...")
-                    download_file(key)
-                    # set it back until it finds the next one
-                found_bucket = False 
-            i += 1
+                # i -= 1
+                
+            # now copy the file to the output bucket and target s3 key
+            for key in old_s3_keys:
+                print(f"found resource to download: {key} in file {filename}. Downloading...")
+                download_file(key)
+                # set it back until it finds the next one
+            found_bucket = False 
+        i += 1
     
     
 
