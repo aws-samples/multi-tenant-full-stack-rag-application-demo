a35339bd085bb1f497d116e66d50b22bebe238da
Dave Thibault <davetbo@amazon.com>
2024-11-02 17:59:40 +0000

    cloudformation export

--- cfn/install.py
+++ cfn/install.py
@@ -18,57 +18,63 @@ input_values = {
     'stack_name': ''
 }
 
+params = [
+    {
+        'ParameterKey': 'allowedEmailDomains',
+        'ParameterValue': input_values['allowed_email_domains']
+    },
+    {
+        'ParameterKey': 'appName',
+        'ParameterValue': input_values['app_name']
+    },
+    {
+        'ParameterKey': 'removalPolicy',
+        'ParameterValue': input_values['removal_policy']
+    },
+    {
+        'ParameterKey': 'signUpEmailBody',
+        'ParameterValue': input_values['signup_email_body']
+    },
+    {
+        'ParameterKey': 'signUpEmailSubject',
+        'ParameterValue': input_values['signup_email_subject']
+    } 
+]
+
 for val in list(input_values.keys()):
     with open(f'.input_values_cache/{val}', 'r') as f:
         input_values[val] = f.read().strip().strip('/')
 
 print(f"Input values are: {input_values}")
 
-stack = cfn.create_stack(
-    StackName=input_values['stack_name'],
-    TemplateURL=f"https://{input_values['output_bucket']}.s3.{region}.amazonaws.com/{input_values['output_prefix']}/mtfsrad-stack.yaml",
-    Parameters=[
-        {
-            'ParameterKey': 'allowedEmailDomains',
-            'ParameterValue': input_values['allowed_email_domains']
-        },
-        {
-            'ParameterKey': 'appName',
-            'ParameterValue': input_values['app_name']
-        },
-        # {
-        #     'ParameterKey': 'CodeBuildRoleArn',
-        #     'ParameterValue': input_values['codebuild_role_arn']
-        # },
-        # {
-        #     'ParameterKey': 'ECRRepoName',
-        #     'ParameterValue': input_values['ecr_repo_name']
-        # },
-        # {
-        #     'ParameterKey': 'OutputBucket',
-        #     'ParameterValue': input_values['output_bucket']
-        # },
-        # {
-        #     'ParameterKey': 'OutputPrefix',
-        #     'ParameterValue': input_values['output_prefix']
-        # },
-        {
-            'ParameterKey': 'removalPolicy',
-            'ParameterValue': input_values['removal_policy']
-        },
-        {
-            'ParameterKey': 'signUpEmailBody',
-            'ParameterValue': input_values['signup_email_body']
-        },
-        {
-            'ParameterKey': 'signUpEmailSubject',
-            'ParameterValue': input_values['signup_email_subject']
-        } 
-    ],
-    TimeoutInMinutes=60,
-    Capabilities=[
-        'CAPABILITY_NAMED_IAM',
-        'CAPABILITY_AUTO_EXPAND'
-    ],
-    EnableTerminationProtection=False
-)
+template_url = f"https://{input_values['output_bucket']}.s3.{region}.amazonaws.com/{input_values['output_prefix']}/mtfsrad-stack.yaml"
+stacks = client.list_stacks()['StackSummaries']
+stack = None
+for existing_stack in stacks:
+    if stack_name.startswith(input_values['stack_name']):
+        stack = existing_stack
+
+if not stack:
+    stack = cfn.create_stack(
+        StackName=input_values['stack_name'],
+        TemplateURL=template_url,
+        Parameters=params,
+        TimeoutInMinutes=60,
+        Capabilities=[
+            'CAPABILITY_NAMED_IAM',
+            'CAPABILITY_AUTO_EXPAND'
+        ],
+        EnableTerminationProtection=False
+    )
+else:
+    stack = cfn.update_stack(
+        StackName=stack['StackName'],
+        TemplateURL=template_url,
+        Parameters=params,
+        TimeoutInMinutes=60,
+                Capabilities=[
+            'CAPABILITY_NAMED_IAM',
+            'CAPABILITY_AUTO_EXPAND'
+        ],
+        EnableTerminationProtection=False
+    )
